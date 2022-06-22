03a7319d88b06257f37c59e0dda7399007b1dd77
Pritam Dash <pritamdash2015@outlook.com>
2022-06-22 21:47:55 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pritam Dash on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
